f87a877eec54745f6b437be60719a27254e70af4
Andre-42 <>
2023-07-25 16:45:07 +0000

    base file structure

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function play() {
+    comsole.log("game running")
+}
+function pause() {
+    console.log("game paused")
+}
+function stop() {
+    console.log("game finished")
+}
+function anyKeyMode() {
+    console.log("AnyKey mode enabled")
+}
