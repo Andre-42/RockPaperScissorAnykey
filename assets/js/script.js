213e061c7826f2b541e38205fae8bce171ba8c16
Andre-42 <>
2023-07-28 15:55:24 +0000

    styling gameplay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
+// event listeners
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    // old syntax: for (let i=0; i<buttons.length; i++){
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "playpause") {
+                alert("You clicked play");
+                
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            
+            }
+        });
+    }
+    // check answer when enter is pressed instead of button
+    //document.getElementById("answer-box").addEventListener("keydown", function (event) {
+    //    console.log(event.key);
+    //    if (event.key === "Enter") {
+    //        checkAnswer();
+    //    }
+    });
+
+    runGame("addition");
+});
 function play() {
     comsole.log("game running")
 }
