b4e487874ed3db9df237549498e24aaf3079f260
Andre-42 <135899198+Andre-42@users.noreply.github.com>
2023-08-06 06:34:44 +0200

    Update script.js function annotation

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,77 +7,49 @@ var loseColor = ["white", "0.5"];
 var drawColor = ["white", "1"];
 var ultimateKeyEnabled = false;
 var keyListenOn = false;
+
 // event listeners
 document.addEventListener("DOMContentLoaded", function () {
-
     let buttons = document.getElementsByTagName("button");
-    // old syntax: for (let i=0; i<buttons.length; i++){
     for (let button of buttons) {
         // click check
         button.addEventListener("click", function () {
-            //if (this.getAttribute("data-type") === "playpause") {
-            //    alert("You clicked play");
-            //    runGame();
-            //} else {
-            //    let gameType = this.getAttribute("data-type");
-            //    alert(`You clicked ${gameType}`);
-            //}
             let todo = this.getAttribute("data-type");
-            console.log(todo);
             runGame(todo);
         });
-
         // hover check
         button.addEventListener("mouseenter", function () {
             let lastActive = this.getAttribute("data-type");
-            console.log("active hover" + lastActive);
-            console.log(((lastActive === "playpause") || (lastActive === "stop") || (lastActive === "anykey") || (lastActive === "rules")));
             if ((lastActive === "playpause") || (lastActive === "stop") || (lastActive === "anykey") || (lastActive === "rules") || (lastActive === "playerid") || (lastActive === "commit-key")) {
             } else {
-                console.log("register time");
                 countTime(lastActive);
             }
         });
         button.addEventListener("mouseleave", function () {
             let lastActive = this.getAttribute("data-type");
-            console.log("active hover" + lastActive);
-            console.log(((lastActive === "playpause") || (lastActive === "stop") || (lastActive === "anykey") || (lastActive === "rules")));
             if ((lastActive === "playpause") || (lastActive === "stop") || (lastActive === "anykey") || (lastActive === "rules") || (lastActive === "playerid") || (lastActive === "commit-key")) {
             } else {
-                console.log("register time");
                 endTime(lastActive);
                 shakeAgain(lastActive);
             }
         });
     }
-    // check answer when enter is pressed instead of button
-    //document.getElementById("answer-box").addEventListener("keydown", function (event) {
-    //    console.log(event.key);
-    //    if (event.key === "Enter") {
-    //        checkAnswer();
-    //    }
-    //});
     // create listener for keybord input
     document.addEventListener("keydown", function (event) {
-        console.log(document.getElementById(event.target.id).value);
-        console.log((ultimateKeyEnabled && keyListenOn))
         if (event.key === "Enter") {
             let findInputBox = document.getElementById(event.target.id);
             commitInput(findInputBox);
         } else if (ultimateKeyEnabled && keyListenOn) {
             todo = keyInputTranslate(event.key);
-            console.log(todo)
             runGame(todo);
         }
     });
-    // create event listener for input enter press
-    //document.getElementById()
-    //runGame();
 });
+/**
+ * The main game function, called when the player starts playing. This function controls the play mode asignments
+ */
 function runGame(todo) {
-    console.log(todo);
     tagfunc = document.getElementById(todo).innerText;
-    console.log(tagfunc)
     if (tagfunc === "play") {
         play();
     } else if (tagfunc === "pause") {
@@ -91,51 +63,54 @@ function runGame(todo) {
     } else if (todo === "playerid") {
         playerId();
     } else {
-        console.log("played " + todo);
+        // players hand selection: console.log("played " + todo);
         scoreHand(todo);
     }
 }
-
+/**
+ * This function changes the play pause button design.
+ */
 function play() {
-    console.log("game running");
-
     currentText = document.getElementById("playicon").className;
-    console.log(currentText);
     if (currentText === "fa-solid fa-circle-play") {
+        // change button to pause mode availability
         document.getElementById("playicon").className = "fa-solid fa-pause";
         document.getElementById("playpause").innerText = "pause";
         document.getElementById("hideplayer").style.display = "none";
         document.getElementById("activeplayer").style.display = "inline-block";
-
+        // shaking hands while player is idle
         document.getElementById("showhand-player").className = "fa-regular fa-hand-back-fist fa-shake";
         document.getElementById("showhand-pc").className = "fa-regular fa-hand-back-fist fa-shake";
-
+        // listen for keyboard input enabled
         keyListenOn = true;
     } else {
-        console.log("Error: play-function");
+        // console.log("Error: play-function");
         keyListenOn = false;
     }
 }
+/**
+ * This function provides the reverse action to play()
+ */
 function pause() {
-    console.log("game paused");
-
     currentText = document.getElementById("playicon").className;
-    console.log(currentText);
     if (currentText === "fa-solid fa-pause") {
         document.getElementById("playicon").className = "fa-solid fa-circle-play";
         document.getElementById("playpause").innerText = "play";
         document.getElementById("hideplayer").style.display = "inline-block";
         document.getElementById("activeplayer").style.display = "none";
-
+        // shaking hands become idle
         document.getElementById("showhand-player").className = "fa-regular fa-hand-back-fist";
         document.getElementById("showhand-pc").className = "fa-regular fa-hand-back-fist";
-
+        // listener for keyboard input is shut down
         keyListenOn = false;
     } else {
-        console.log("Error: pause function");
+        // console.log("Error: pause function");
         keyListenOn = false;
     }
 }
+/**
+ * The stop function stops the game and resets the counter
+ */
 function stop() {
     console.log("game finished");
     keyListenOn = false;
@@ -144,19 +119,21 @@ function stop() {
     document.getElementById("playpause").innerText = "play";
     document.getElementById("hideplayer").style.display = "inline-block";
     document.getElementById("activeplayer").style.display = "none";
-
+    // reset scoreboard
     document.getElementById("c-win").innerHTML = 0;
     document.getElementById("c-played").innerHTML = 0;
     document.getElementById("w-game").innerHTML = 0;
     document.getElementById("l-game").innerHTML = 0;
     document.getElementById("w-set").innerHTML = 0;
     document.getElementById("l-set").innerHTML = 0;
-
+    // shaking hands become idle
     document.getElementById("showhand-player").className = "fa-regular fa-hand-back-fist";
     document.getElementById("showhand-pc").className = "fa-regular fa-hand-back-fist";
 }
+/**
+* activation of the anykef tracking feature
+**/
 function anyKeyMode() {
-    console.log("AnyKey mode enabled");
     if (ultimateKeyEnabled) {
         ultimateKeyEnabled = false;
         document.getElementsByClassName("btn btn-anykey")[0].style.backgroundColor = "floralWhite";
@@ -165,10 +142,10 @@ function anyKeyMode() {
         ultimateKeyEnabled = true;
         document.getElementsByClassName("btn btn-anykey")[0].style.backgroundColor = "cadetBlue";
     }
-    
-    
-
 }
+/**
+* This function translates the user input by keyboard as defined in the rules section to the corresponding hand signals
+**/
 function keyInputTranslate(selectedKey) {
     selectedKey = selectedKey.toLowerCase();
     let activity = "playpause";
@@ -190,7 +167,9 @@ function keyInputTranslate(selectedKey) {
     console.log(activity)
     return activity;
 }
-
+/**
+* This function allows keyboard adjustments within rules section
+**/
 function commitInput(idTag) {
     changedItem = idTag.value;
     let selectedInput = NaN;
@@ -230,6 +209,9 @@ function commitInput(idTag) {
         }
     }
 }
+/**
+* Changes the name tag assembly for the player between edit- and display-mode
+**/
 function playerId() {
     getIcon = document.getElementById("player-name-save").className;
     if (getIcon === "fa-solid fa-pen-to-square") {
@@ -243,6 +225,9 @@ function playerId() {
         changePlayerName();
     }
 }
+/**
+* checks the new name. If it is empty is not changed.
+**/
 function changePlayerName() {
     let newName = document.getElementById('inputTextField').value;
     let oldName = document.getElementById('playerid').innerText;
@@ -250,6 +235,10 @@ function changePlayerName() {
         document.getElementById('playerid').innerText = newName;
     }
 }
+// game relevant functions
+/**
+* This function writes the current hover time into the html on an invisible field
+**/
 function countTime(hoverButton) {
     let checkHand = hoverButton === document.getElementById("lastActive").innerHTML;
     document.getElementById("lastActive").innerHTML = hoverButton;
@@ -258,6 +247,9 @@ function countTime(hoverButton) {
         document.getElementById("endTime").innerHTML = NaN;
     }
 }
+/**
+* This function records the exit from play relevant buttons
+**/
 function endTime(hoverButton) {
     let checkHand = hoverButton === document.getElementById("lastActive").innerHTML;
     if (checkHand) {
@@ -265,17 +257,21 @@ function endTime(hoverButton) {
     }
     console.log(checkHand);
 }
+/**
+* function regulates the win or lose execution
+**/
 function scoreHand(player) {
+    // get player score
     let score = parseInt(document.getElementById(player + 'Num').innerText); // reads string and makes integer
     document.getElementById(player + 'Num').textContent = ++score;
     endTime(player);
+    // pc choice comes next
     let timeTaken = document.getElementById("endTime").innerHTML;
     let pc = pcHand(timeTaken);
-    console.log("pc:" + pc + " vs player:" + player);
+    // console.log("pc:" + pc + " vs player:" + player);
     // check for winner
     let gamescore = findWinner(player, pc);
-    console.log(gamescore);
-
+    // visual changes to the game area
     if (gamescore > 0) {
         let win = parseInt(document.getElementById('c-win').innerText);
         document.getElementById("c-win").innerHTML = ++win;
@@ -283,32 +279,27 @@ function scoreHand(player) {
         document.getElementById("c-played").innerHTML = ++win;
         win = parseInt(document.getElementById('w-game').innerText);
         document.getElementById("w-game").innerHTML = ++win;
-
         resultColor(winColor, loseColor);
     } else if (gamescore < 0) {
         let loss = parseInt(document.getElementById('c-played').innerText);
         document.getElementById("c-played").innerHTML = ++loss;
         loss = parseInt(document.getElementById('l-game').innerText);
         document.getElementById("l-game").innerHTML = ++loss;
-
         resultColor(loseColor, winColor);
     } else {
         let loss = parseInt(document.getElementById('c-played').innerText);
         document.getElementById("c-played").innerHTML = ++loss;
-
         resultColor(drawColor, drawColor);
     }
     let cSet = parseInt(document.getElementById("c-win").innerText);
     let cGame = parseInt(document.getElementById("c-played").innerText);
+    // Set score
     let cDiff = cGame - cSet;
-
-    console.log("diff" + cDiff);
     if (cDiff >= 3) {
         let loss = parseInt(document.getElementById("l-set").innerText);
         document.getElementById("l-set").innerHTML = ++loss;
         document.getElementById("c-played").innerHTML = 0;
         document.getElementById("c-win").innerHTML = 0;
-
         let sum = parseInt(document.getElementById("w-set").innerText);
         let win = parseInt(document.getElementById("w-set").innerText);
         sum = sum + parseInt(document.getElementById("l-set").innerText);
@@ -319,26 +310,26 @@ function scoreHand(player) {
         document.getElementById("w-set").innerHTML = ++win;
         document.getElementById("c-played").innerHTML = 0;
         document.getElementById("c-win").innerHTML = 0;
-
         let sum = parseInt(document.getElementById("w-set").innerText);
         sum = sum + parseInt(document.getElementById("l-set").innerText);
         let avScore = Math.round((win / sum) * 10000) / 10000;
         document.getElementById("av-score").innerHTML = avScore;
     } else if (cGame == 5) {
-        console.log("5 games found.");
         document.getElementById("c-played").innerHTML = 0;
         document.getElementById("c-win").innerHTML = 0;
-    } else {
-        console.log("undefined score condition...");
     }
 }
+/**
+* PC decision function
+**/
 function pcHand(timeTaken) {
+    // if PC is listening in
     let bestChance;
     if (ultimateKeyEnabled) {
         let lastSelected = document.getElementById("lastActive").innerHTML;
         let dontChoose = document.getElementsByClassName(lastSelected + "-win");
         let sumChance = [lastSelected];
-        
+        // select winning moves to user input
         for (let looseItem of dontChoose) {
             for (let handOption of keyName) {
                 if (!(looseItem.id === handOption)) {
@@ -346,19 +337,19 @@ function pcHand(timeTaken) {
                 }
             }
         }
-        
+        // random choice of one.
         let sumChanceId = Math.ceil(Math.random() * sumChance.length) - 1;
         bestChance = sumChance[sumChanceId];
     }
     let drawOptions = drawHistory;
+    // time dependant decision if the user is to slow the pc will find out and update its choice
     if (timeTaken > 0.5) {
         drawOptions.push(bestChance[0]);
     }
-    
+    // random selector for pc
     let pcHandRand = Math.ceil(Math.random() * drawOptions.length) - 1;
-    console.log(pcHandRand);
     let draw = drawOptions[pcHandRand];
-
+    // user reaction is remembered for the next moves
     if (ultimateKeyEnabled) {
         drawHistory.push(bestChance[0]);
         if (drawHistory.length>=100) {
@@ -368,6 +359,9 @@ function pcHand(timeTaken) {
     console.log(drawHistory);
     return draw;
 }
+/**
+* this function will show the results of the match after hand selection
+**/
 function findWinner(player, pc) {
     let playerStrength = document.getElementsByClassName(player + "-win");
     let playerWins = -1;
@@ -418,31 +412,30 @@ function findWinner(player, pc) {
         document.getElementById("showhand-pc-img").src = "";
         document.getElementById("showhand-pc-img").alt = "";
     }
-
     scorePlayer = playerWins;
     return scorePlayer;
 }
+/**
+* background color change of result icon
+**/
 function resultColor(playerColor, pcColor) {
-
     document.getElementById("showhand-player").style.backgroundColor = playerColor[0];
     document.getElementById("showhand-player-img").style.backgroundColor = document.getElementById("showhand-player").style.backgroundColor;
     document.getElementById("showhand-player").style.opacity = playerColor[1];
     document.getElementById("showhand-player-img").style.opacity = document.getElementById("showhand-player").style.opacity;
-
     document.getElementById("showhand-pc").style.backgroundColor = pcColor[0];
     document.getElementById("showhand-pc-img").style.backgroundColor = document.getElementById("showhand-player").style.backgroundColor;
     document.getElementById("showhand-pc").style.opacity = pcColor[1];
     document.getElementById("showhand-pc-img").style.backgroundColor = document.getElementById("showhand-player").style.opacity;
 }
+/**
+* shake response during gameplay
+**/
 function shakeAgain(lastActive) {
-    //let checkHand = hoverButton === document.getElementById("lastActive").innerHTML;
-    //if (checkHand) {
     document.getElementById("showhand-player").className = "fa-regular fa-hand-back-fist fa-shake";
     document.getElementById("showhand-player-img").src = "";
     document.getElementById("showhand-player-img").alt = "";
-
     document.getElementById("showhand-pc").className = "fa-regular fa-hand-back-fist fa-shake";
     document.getElementById("showhand-pc-img").src = "";
     document.getElementById("showhand-pc-img").alt = "";
-    //}
 }
